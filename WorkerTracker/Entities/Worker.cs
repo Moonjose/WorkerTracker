0fa7b5d163ec14dee2b4e81b5758ec36bbbb4d12
Jose <ajose9975@gmail.com>
2022-11-08 13:29:12 -0300

    Add project files.

---
+++ WorkerTracker/Entities/Worker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkerTracker.Entities.Enums;
+
+namespace WorkerTracker.Entities {
+    internal class Worker {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker() {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department) {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract hourContract) {
+            Contracts.Add(hourContract);
+        }
+
+        public void RemoveContract(HourContract hourContract) {
+            Contracts.Remove(hourContract);
+        }
+
+        public double Income(int year, int month) {
+            double sum = BaseSalary;
+
+            foreach(HourContract contract in Contracts) {
+                if(contract.Date.Year == year && contract.Date.Month == month) {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
